2adcc26ddca77bab5914570ee514f6e3ab2daecb
Muhammad Anas Saud <mohammad.anas190@gmail.com>
2023-10-03 00:56:58 +0500

    feat: radio button component

---
+++ src/components/ui/RadioGroup.tsx
@@ -0,0 +1,71 @@
+import {
+  RadioGroup as BaseRadioGroup,
+  Radio as BaseRadio,
+  RadioGroupProps as BaseRadioGroupProps,
+  RadioProps as BaseRadioProps,
+} from "@nextui-org/radio";
+// import { cn } from "@nextui-org/react";
+import { cva } from "class-variance-authority";
+
+interface RadioGroupProps extends BaseRadioGroupProps {
+  type?: "card";
+}
+interface RadioProps extends BaseRadioProps {
+  type?: "card";
+}
+
+const RadioGroup = ({ type, ...props }: RadioGroupProps) => {
+  const RadioGroupVariants = cva(
+    /* RadioGroup base style */
+    "h-fit",
+    {
+      variants: {
+        /* RadioGroup colors */
+        intent: {
+          card: "py-4 px-4",
+        },
+      },
+
+      // defaults
+      defaultVariants: {
+        intent: "card",
+      },
+    }
+  );
+  return (
+    <BaseRadioGroup
+      classNames={{
+        base: RadioGroupVariants({ intent: type }),
+      }}
+      {...props}
+    />
+  );
+};
+
+RadioGroup.displayName = "RadioGroup";
+
+// eslint-disable-next-line react/display-name
+RadioGroup.Radio = ({ type, ...props }: RadioProps) => {
+  const RadioVariants = cva("", {
+    variants: {
+      intent: {
+        card: [
+          "inline-flex m-0 bg-content1 hover:bg-content2 items-center justify-between",
+          "flex-row-reverse max-w-[300px] cursor-pointer rounded-lg gap-4 p-4 border-2 border-dashed",
+          "data-[selected=true]:border-primary data-[selected=true]:border-solid",
+        ],
+      },
+    },
+  });
+
+  return (
+    <BaseRadio
+      {...props}
+      classNames={{
+        base: RadioVariants({ intent: type }),
+      }}
+    />
+  );
+};
+
+export { RadioGroup };
